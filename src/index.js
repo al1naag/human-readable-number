9b862457579ca454ce91dc853fa81fd2c4c50997
Alina Lipova <alina.ag@gmail.com>
2022-07-02 23:52:16 +0500

    init: human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    let nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 20)
+        return nums[number];
+
+    let x = number % 10;
+
+    if (number < 100)
+        return tens[Math.floor(number / 10) - 2] + (x ? " " + nums[x] : "");
+    else if (number < 1000)
+        return nums[Math.floor(number / 100)] + " hundred" + (number % 100 == 0 ? "" : " " + toReadable(number % 100));
 }
